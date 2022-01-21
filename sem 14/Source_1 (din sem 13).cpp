141bfcb8a961e7c4ced50545dd811ae5e5ef678c
raresciobanu <raresciobanu95@gmail.com>
2022-01-21 15:26:35 +0200

    sem 14

---
+++ sem 14/Source_1 (din sem 13).cpp
@@ -0,0 +1,367 @@
+﻿#include<iostream>
+#include<fstream>
+#include<vector>
+
+using namespace std;
+
+class GestiuneExamene {
+private:
+	static int exameneActive;
+protected:
+	string disciplina;
+	double nota;
+	string dataExamen;
+	const int ID;
+public:
+	GestiuneExamene() :ID(1002) {
+		disciplina = "POO";
+		nota = 4;
+		dataExamen = "20.02.2022";
+	}
+
+	GestiuneExamene(string disciplina, double nota, string dataExamen, int ID) :ID(ID) {
+		this->disciplina = disciplina;
+		this->nota = nota;
+		this->dataExamen = dataExamen;
+		exameneActive++;
+	}
+
+	GestiuneExamene(const GestiuneExamene& g) :ID(g.ID) {
+		this->disciplina = g.disciplina;
+		this->nota = g.nota;
+		this->dataExamen = g.dataExamen;
+		exameneActive++;
+	}
+
+	~GestiuneExamene() {
+
+	}
+
+	GestiuneExamene& operator=(const GestiuneExamene& g) {
+		if (this != &g) {
+			this->disciplina = g.disciplina;
+			this->nota = g.nota;
+			this->dataExamen = g.dataExamen;
+		}
+
+		return *this;
+	}
+
+	friend ostream& operator<<(ostream& out, const GestiuneExamene& g) {
+		out << "ID: " << g.ID << endl;
+		out << "Disciplina: " << g.disciplina << endl;
+		out << "Nota: " << g.nota << endl;
+		out << "Data examen: " << g.dataExamen << endl;
+
+		return out;
+	}
+
+	bool operator==(GestiuneExamene& g) {
+		if (this == &g) {
+			return true;
+		}
+
+		return this->nota == g.nota && disciplina == g.disciplina && dataExamen == g.dataExamen;
+	}
+
+	bool operator>=(GestiuneExamene& g) {
+		return this->nota >= g.nota;
+	}
+
+	operator double() {
+		return nota;
+	}
+
+	//pre ++
+	GestiuneExamene& operator++() {
+		if (nota > 0 && nota <= 9) {
+			nota++;
+		}
+
+		return *this;
+	}
+
+	//post ++
+	GestiuneExamene operator++(int) {
+		GestiuneExamene aux(*this);
+
+		if (nota > 0 && nota <= 9) {
+			nota++;
+		}
+
+		return aux;
+	}
+
+	string getDisciplina() { return disciplina; }
+
+	double getNota() { return nota; }
+
+	string getDataExamen() { return dataExamen; }
+
+	void platesteRestanta() {
+		cout << "Restanta a fost platita\n";
+	}
+
+	double afiseazaNotaExamen() {
+		return nota;
+	}
+
+	friend ofstream& operator<<(ofstream& out, const GestiuneExamene& g) {
+		out.write((char*)&g.nota, sizeof(double));
+
+		int dim = 0;
+		dim = g.disciplina.size() + 1;
+
+		out.write((char*)&dim, sizeof(int));
+		out.write(g.disciplina.c_str(), dim);
+
+		dim = g.dataExamen.size() + 1;
+		out.write((char*)&dim, sizeof(int));
+		out.write(g.dataExamen.c_str(), dim);
+
+		return out;
+	}
+
+	friend ifstream& operator>>(ifstream& in, GestiuneExamene& g) {
+		in.read((char*)&g.nota, sizeof(double));
+
+		int dim = 0;
+		in.read((char*)&dim, sizeof(int));
+
+		char aux[100];
+		in.read(aux, dim);
+		g.disciplina = aux;
+
+		dim = 0;
+		in.read((char*)&dim, sizeof(int));
+		in.read(aux, dim);
+		g.dataExamen = aux;
+
+		return in;
+	}
+};
+int GestiuneExamene::exameneActive = 0;
+
+class Catalog {
+	GestiuneExamene* examene;
+	int nrExamene;
+public:
+
+	Catalog(GestiuneExamene* examene, int nrExamene) {
+		this->nrExamene = nrExamene;
+		this->examene = new GestiuneExamene[nrExamene];
+		for (int i = 0; i < nrExamene; i++) {
+			this->examene[i] = examene[i];
+		}
+	}
+
+	Catalog(const Catalog& c) {
+		this->nrExamene = c.nrExamene;
+		this->examene = new GestiuneExamene[c.nrExamene];
+		for (int i = 0; i < c.nrExamene; i++) {
+			this->examene[i] = c.examene[i];
+		}
+	}
+
+	~Catalog() {
+		if (examene != nullptr) {
+			delete[] examene;
+		}
+	}
+
+	void operator+=(GestiuneExamene g) {
+		GestiuneExamene* aux = new GestiuneExamene[nrExamene + 1];
+		for (int i = 0; i < nrExamene; i++) {
+			aux[i] = examene[i];
+		}
+
+		aux[nrExamene] = g;
+		delete[] examene;
+		nrExamene++;
+		examene = aux;
+	}
+
+	void operator-=(GestiuneExamene examen) {
+		int ok = 0;
+		int duplicate = 0;
+		for (int i = 0; i < nrExamene; i++) {
+			if (examene[i] == examen) {
+				ok = 1;
+				duplicate++;
+			}
+		}
+
+		if (ok == 1) {
+			GestiuneExamene* aux = new GestiuneExamene[nrExamene - duplicate];
+			for (int i = 0; i < nrExamene; i++) {
+				if (!(examene[i] == examen)) {
+					aux[i] = examene[i];
+				}
+			}
+			nrExamene -= duplicate;
+			delete[] examene;
+			examene = aux;
+		}
+	}
+
+	friend ostream& operator<< (ostream& out, const Catalog& catalog) {
+		out << "\n\nCATALOG\n\n";
+		out << catalog.nrExamene << endl;
+		for (int i = 0; i < catalog.nrExamene; i++) {
+			out << catalog.examene[i] << endl;
+		}
+
+		return out;
+	}
+};
+
+
+//template
+template <class T> class CatalogTemplate {
+	T* examene;
+	int nrExamene;
+public:
+
+	CatalogTemplate(T* examene, int nrExamene) {
+		this->nrExamene = nrExamene;
+		this->examene = new T[nrExamene];
+		for (int i = 0; i < nrExamene; i++) {
+			this->examene[i] = examene[i];
+		}
+	}
+
+	CatalogTemplate(const Catalog& c) {
+		this->nrExamene = c.nrExamene;
+		this->examene = new T[c.nrExamene];
+		for (int i = 0; i < c.nrExamene; i++) {
+			this->examene[i] = c.examene[i];
+		}
+	}
+
+	~CatalogTemplate() {
+		if (examene != nullptr) {
+			delete[] examene;
+		}
+	}
+
+	void operator+=(T g) {
+		T* aux = new T[nrExamene + 1];
+		for (int i = 0; i < nrExamene; i++) {
+			aux[i] = examene[i];
+		}
+
+		aux[nrExamene] = g;
+		delete[] examene;
+		nrExamene++;
+		examene = aux;
+	}
+
+	void operator-=(T examen) {
+		int ok = 0;
+		int duplicate = 0;
+		for (int i = 0; i < nrExamene; i++) {
+			if (examene[i] == examen) {
+				ok = 1;
+				duplicate++;
+			}
+		}
+
+		if (ok == 1) {
+			T* aux = new T[nrExamene - duplicate];
+			for (int i = 0; i < nrExamene; i++) {
+				if (!(examene[i] == examen)) {
+					aux[i] = examene[i];
+				}
+			}
+			nrExamene -= duplicate;
+			delete[] examene;
+			examene = aux;
+		}
+	}
+
+	friend ostream& operator<< (ostream& out, const CatalogTemplate& catalog) {
+		out << "\n\nCATALOG\n\n";
+		out << catalog.nrExamene << endl;
+		for (int i = 0; i < catalog.nrExamene; i++) {
+			out << catalog.examene[i] << endl;
+		}
+
+		return out;
+	}
+};
+void main() {
+	GestiuneExamene g1;
+	GestiuneExamene g2(g1);
+
+	GestiuneExamene g3("MKT", 7.4, "10.02.2022", 1112);
+	GestiuneExamene g4("MAN", 9.3, "10.02.2022", 1113);
+	GestiuneExamene g5("CIBE", 9.9, "10.02.2022", 1114);
+	//pre
+	cout << ++g3 << endl;
+	//post
+	cout << g3++ << endl;
+	cout << g3 << endl;
+	//cast
+	double dd = g3; //cast implicit
+	cout << (double)g3 << endl;//cast explicit
+
+	vector<GestiuneExamene> gestiuneExameneVector;
+	gestiuneExameneVector.push_back(g1);
+	gestiuneExameneVector.push_back(g2);
+	gestiuneExameneVector.push_back(g3);
+	gestiuneExameneVector.push_back(g4);
+	gestiuneExameneVector.push_back(g5);
+
+	ofstream fout("fout.bin", ios::binary | ios::out);
+	if (fout.is_open()) {
+		int dimVector = gestiuneExameneVector.size();
+		fout.write((char*)&dimVector, sizeof(int));
+
+		for (int i = 0; i < gestiuneExameneVector.size(); i++) {
+			fout << gestiuneExameneVector[i];
+		}
+
+		fout.close();
+	}
+	else {
+		cout << "Fisierul nu a putut fi deschis pt scriere. \n";
+	}
+
+
+	vector<GestiuneExamene> gestiuneExameneVectorDinFisier;
+	ifstream fin("fout.bin", ios::binary | ios::in);
+	if (fin.is_open()) {
+		int dimVector = 0;
+		fin.read((char*)&dimVector, sizeof(int));
+
+		for (int i = 0; i < dimVector; i++) {
+			GestiuneExamene uuu;
+			fin >> uuu;
+
+
+			gestiuneExameneVectorDinFisier.push_back(uuu);
+		}
+
+		fin.close();
+	}
+	else {
+		cout << "Fisierul nu a putut fi deschis pt citire. \n";
+	}
+
+	cout << "\n\nafisare din fisier binar\n\n";
+	for (int i = 0; i < gestiuneExameneVectorDinFisier.size(); i++) {
+		cout << gestiuneExameneVectorDinFisier[i] << endl;
+	}
+
+	//-= +=
+	Catalog catalog(new GestiuneExamene[3]{ g3,g4,g5 }, 3);
+	cout << catalog;
+	catalog += g5;
+	cout << catalog;
+	catalog -= g5;
+	cout << catalog;
+
+	cout << "\n\ntemplate\n\n";
+	CatalogTemplate<GestiuneExamene> catalogTemplate(new GestiuneExamene[2]{ g3,g5 }, 2);
+	cout << catalogTemplate;
+}
---
+++ sem 14/Source_2 .cpp
@@ -0,0 +1,198 @@
+﻿#include<iostream>
+#include<fstream>
+#include<vector>
+
+using namespace std;
+
+
+class ContBancar {
+	string numar;
+	string titular;
+	string parola;
+	double sold;
+	string valuta;
+
+public:
+	ContBancar() {
+		numar = "";
+		titular = "";
+		parola = "";
+		sold = 0;
+		valuta = "";
+	}
+
+	ContBancar(string numar, string titular, string parola, string valuta, double sold) {
+		this->numar = numar;
+		this->titular = titular;
+		this->parola = parola;
+		this->valuta = valuta;
+		this->sold = sold;
+	}
+
+	ContBancar& operator=(ContBancar& cb) {
+		if (this != &cb) {
+			if (valuta == cb.valuta) {
+				this->sold += cb.sold;
+			}
+			else {
+				if (valuta == "RON" && cb.valuta == "DOL") {
+					this->sold += cb.sold * 4.2;//curs 4.2 ron/dol
+				}
+				else {
+					this->sold += cb.sold / 4.2;
+				}
+			}
+			cb.sold = 0;
+		}
+
+		return *this;
+	}
+
+	//contbancar+STL
+	//ContBancar operator+(vector<ContBancar>& conturi) {
+	//	ContBancar cb(*this);
+	//	for (int i = 0; i < conturi.size(); i++) {
+	//		cb.sold += conturi[i].sold;
+	//		conturi[i].sold = 0;
+	//	}
+
+	//	return cb;
+	//}
+
+	//void operator+=(ContBancar cont) {
+	//	this->sold += cont.sold;
+	//}
+
+	friend ostream& operator<<(ostream& out, const ContBancar& cb) {
+		cout << "numar = " << cb.numar << endl;
+		cout << "titular = " << cb.titular << endl;
+		cout << "parola = " << cb.parola << endl;
+		cout << "sold = " << cb.sold << endl;
+		cout << "valuta = " << cb.valuta << endl << endl;
+
+		return out;
+	}
+
+	friend istream& operator>>(istream& in, ContBancar& cb) {
+		char aux[200];
+		cout << "numar = ";
+		in.getline(aux, 200);
+		cb.numar = aux;
+
+		cout << "titular = ";
+		in.getline(aux, 200);
+		cb.titular = aux;
+
+		//cout << "parola = "; //fara ecou pt parola
+		in.getline(aux, 200);
+		cb.parola = aux;
+
+		cout << "sold = ";
+		in >> cb.sold;
+
+		in.ignore();
+		cout << "valuta = ";
+		in.getline(aux, 200);
+		cb.valuta = aux;
+
+
+		return in;
+	}
+
+	friend ofstream& operator<<(ofstream& out, const ContBancar& cb) {
+		int dim = 0;
+
+		dim = cb.numar.size() + 1;
+		out.write((char*)&dim, sizeof(int));
+		out.write(cb.numar.c_str(), dim);
+
+		dim = cb.titular.size() + 1;
+		out.write((char*)&dim, sizeof(int));
+		out.write(cb.titular.c_str(), dim);
+
+		dim = cb.parola.size() + 1;
+		out.write((char*)&dim, sizeof(int));
+		out.write(cb.parola.c_str(), dim);
+
+		out.write((char*)&cb.sold, sizeof(double));
+
+		dim = cb.valuta.size() + 1;
+		out.write((char*)&dim, sizeof(int));
+		out.write(cb.valuta.c_str(), dim);
+
+		return out;
+	}
+
+
+	friend ifstream& operator>>(ifstream& in, ContBancar& cb) {
+		int dim = 0;
+		char aux[200];
+
+		in.read((char*)&dim, sizeof(int));
+		in.read(aux, dim);
+		cb.numar = aux;
+
+		in.read((char*)&dim, sizeof(int));
+		in.read(aux, dim);
+		cb.titular = aux;
+
+		in.read((char*)&dim, sizeof(int));
+		in.read(aux, dim);
+		cb.parola = aux;
+
+		in.read((char*)&cb.sold, sizeof(double));
+
+		in.read((char*)&dim, sizeof(int));
+		in.read(aux, dim);
+		cb.valuta = aux;
+
+		return in;
+	}
+
+	~ContBancar() {
+		ofstream fout("banca.dat", ios::binary | ios::app);
+		if (fout.is_open()) {
+			fout << *this;
+			fout.close();
+		}
+		else {
+			cout << "Eroare banca.dat\n";
+		}
+	}
+};
+
+void main() {
+	ContBancar cb1("RNCB03434 433 4343 4334", "Ion Popescu", "1224", "RON", 2000.59);
+	ContBancar cb2;
+	ContBancar cb3("RNCB04476 999 2223 3232", "George Popescu", "9999", "DOL", 12500.99);
+
+	//	cin >> cb2;
+
+	ofstream fout("banca.dat", ios::binary);
+	if (fout.is_open()) {
+		fout << cb1;
+		fout << cb3;
+		fout.close();
+	}
+	else {
+		cout << "Eroare banca.dat\n";
+	}
+
+
+	ContBancar cb4;
+	ContBancar cb5;
+	ifstream fin("banca.dat", ios::binary);
+	if (fin.is_open()) {
+		fin >> cb4;
+		fin >> cb5;
+		fin.close();
+
+		cout << cb4 << endl << endl << cb5 << endl << endl;
+	}
+	else {
+		cout << "Eroare banca.dat\n";
+	}
+
+	cb1 = cb3;
+	cout << cb1 << endl;
+}
